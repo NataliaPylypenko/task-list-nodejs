bfed5c4f53b22f4f1bbe75c5641d5cc8ee06e4d5
NataliaPylypenko <natalia28ivanivna@gmail.com>
2022-04-18 14:30:22 +0200

    make validator in another file

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -4,6 +4,8 @@ import {getRandomId} from "../helpers/functions";
 import moment from "moment";
 import {TaskInterfaceSummary} from "../types/tasks";
 import {object, string} from "yup";
+import {validateCreateTask} from "../services/validateCreateTask";
+import {validateEditTask} from "../services/validateEditTask";
 
 const router = Router()
 
@@ -26,7 +28,6 @@ router.get('/tasks/:id', ((req, res, next) => {
     const id = req.params.id
 
     const idx = tasks.findIndex(task => task.id === id);
-
     if (idx === -1) res.status(404).send('Task not found')
 
     res.json(tasks[idx])
@@ -37,7 +38,6 @@ router.delete('/tasks/:id', ((req, res, next) => {
     const id = req.params.id
 
     const idx = tasks.findIndex(task => task.id === id);
-
     if (idx === -1) res.status(404).send('Task not found')
 
     tasks.splice(idx, 1)
@@ -45,55 +45,30 @@ router.delete('/tasks/:id', ((req, res, next) => {
     res.status(204).send('No content')
 }))
 
-router.post('/tasks', (async (req, res, next) => {
-    try {
-        const taskSchema = object({
-            name: string().required(),
-            content: string().required(),
-            category: string().required().oneOf(CATEGORIES),
-        });
+router.post('/tasks', validateCreateTask, ((req, res, next) => {
 
-        await taskSchema.validate(req.body);
-
-        const newTask = {
-            ...req.body,
-            id: getRandomId(),
-            created: moment().format('MMMM DD, YYYY'),
-            status: STATUS.ACTIVE
-        }
-
-        tasks.push(newTask)
-        res.status(201).json(newTask)
-    } catch (e) {
-        res.status(422).send(e.message)
+    const newTask = {
+        ...req.body,
+        id: getRandomId(),
+        created: moment().format('MMMM DD, YYYY'),
+        status: STATUS.ACTIVE
     }
+
+    tasks.push(newTask)
+    res.status(201).json(newTask)
 
 }))
 
-router.patch('/tasks/:id', (async (req, res, next) => {
-    try {
-        const taskEditSchema = object({
-            name: string().required(),
-            content: string().required(),
-            category: string().required().oneOf(CATEGORIES),
-            status: string().required().oneOf([STATUS.ACTIVE, STATUS.ARCHIVE]),
-        });
+router.patch('/tasks/:id', validateEditTask, (async (req, res, next) => {
 
-        await taskEditSchema.validate(req.body);
+    const id = req.params.id
 
-        const id = req.params.id
+    const idx = tasks.findIndex(task => task.id === id);
+    if (idx === -1) res.status(404).send('Task not found')
 
-        const idx = tasks.findIndex(task => task.id === id);
-        if (idx === -1) res.status(404).send('Task not found')
+    tasks[idx] = {...tasks[idx], ...req.body};
 
-        tasks[idx] = {...tasks[idx], ...req.body};
-
-        res.json(tasks[idx])
-
-
-    }catch (e){
-        res.status(422).send(e.message)
-    }
+    res.json(tasks[idx])
 
 }))
 
---
+++ src/services/validateCreateTask.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import {object, string} from "yup";
+import {CATEGORIES} from "../db/db";
+
+export const validateCreateTask =  async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const taskSchema = object({
+            name: string().required(),
+            content: string().required(),
+            category: string().required().oneOf(CATEGORIES),
+        });
+
+        await taskSchema.validate(req.body);
+    } catch (e) {
+        res.status(422).send(e.message)
+    }
+
+    next()
+}
