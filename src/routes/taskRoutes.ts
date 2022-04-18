ec94cc95f8cf3e01362576947bc0f7b92e48a495
NataliaPylypenko <natalia28ivanivna@gmail.com>
2022-04-18 13:19:48 +0200

    added get by id added delete task

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -7,4 +7,28 @@ router.get('/tasks', ((req, res, next) => {
     res.json(tasks)
 }))
 
+router.get('/tasks/:id', ((req, res, next) => {
+    const id = req.params.id
+
+    const idx = tasks.findIndex(task => task.id === id);
+
+    if(idx === -1) res.status(404).send('Task not found')
+
+    res.json(tasks[idx])
+}))
+
+
+
+router.delete('/tasks/:id', ((req, res, next) => {
+    const id = req.params.id
+
+    const idx = tasks.findIndex(task => task.id === id);
+
+    if(idx === -1) res.status(404).send('Task not found')
+
+    tasks.splice(idx, 1)
+
+    res.status(204).send('No content')
+}))
+
 export default router
